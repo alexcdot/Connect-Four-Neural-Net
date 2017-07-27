554eba975a13048b47cebe88bc44820113982ebc
alexcdot <alexbroguy@gmail.com>
2017-07-26 19:55:20 -0700

    upload code, first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Connect-Four-Neural-Net
+Our machine learning models for a game of connect four
+
+We use a three layer neural network to create a model to predict whether the current board is a likely win, draw or loss for player x. The 6 x 7 board has been unrolled into a 42 column vector, with 67,000 training examples.
+
+The accuracy thus far with multi-class classification is 78.5%.
---
+++ keras_connect_four_network.py
@@ -0,0 +1,65 @@
+# Connect 4 machine learning model
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
+
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load connect 4 dataset
+dataset = numpy.genfromtxt("connect-4.csv", dtype='str', delimiter=",")
+# split into input (X) and output (Y) variables
+preX = dataset[:,0:42]
+print preX
+preY = dataset[:,42]
+print preY
+
+X = numpy.zeros(preX.shape)
+Y = numpy.zeros(preY.shape)
+
+for i, row in enumerate(preX):
+    for j, col in enumerate(row):
+        if col == 'x':
+            X[i,j] = 1.0
+        if col == 'o':
+            X[i,j] = -1.0
+        if col == 'b':
+            X[i,j] = 0.0
+            
+for i, row in enumerate(preY):
+    if row == 'win':
+        Y[i] = 1.0
+    if row == 'loss':
+        Y[i] = 0.0
+    if row == 'draw':
+        Y[i] = 0.5
+
+encoder = LabelEncoder()
+encoder.fit(Y)
+encoded_Y = encoder.transform(Y)
+dummy_y = np_utils.to_categorical(encoded_Y)
+
+# define baseline model
+def baseline_model():
+    model = Sequential()
+    model.add(Dense(8, input_dim=42, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+estimator = KerasClassifier(build_fn=baseline_model, epochs = 10, batch_size=5,verbose=1)
+kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
+
+results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+
+model.save('connect_four_model.h5')
+
+# accuracy of 78%
